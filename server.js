61acf73fdfc6b5e423414386ba78dbf2dc0cba4c
MrHuguitos <155857650+MrHuguitos@users.noreply.github.com>
2025-06-07 12:45:49 -0300

    Atualização para o Vercel

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import path from "path";
 import routes from "./src/routes/tarefasRoutes.js";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.static(path.resolve("front")));
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando estiver ativo
-app.listen(3000, () => {
-    console.log("server escutando...");
+app.listen(PORT, () => {
+    console.log(`server escutando na porta ${PORT}...`);
 });
 
 routes(app);
